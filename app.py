5a02b7deff1a68f0229e75b5a16db6baebfed9ab
Emma <70605739+Emma-Perez-Rovira@users.noreply.github.com>
2024-08-09 14:15:36 -0400

    Merged with the admin control panel

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, render_template, jsonify
+from flask import Flask, redirect, render_template, jsonify, request
 import random
 import json
+from flask_wtf import FlaskForm
+from wtforms import DecimalRangeField
+import time
 
 # Initialize flask app
 app = Flask(__name__)
@@ -26,6 +29,94 @@ svo2 = 83.0
 delta_p = 11
 rpm = 4000
 
+# Back end code
+
+def cleanValues():
+    global flow_rate
+    global p_ven
+    global p_int
+    global p_art
+    global t_art
+    global svo2
+    global delta_p
+    global rpm
+    flow_rate = float(flow_rate)
+    p_ven = float(p_ven)
+    p_int = float(p_int)
+    p_art = float(p_art)
+    t_art = float(t_art)
+    svo2 = float(svo2)
+    delta_p = float(delta_p)
+    rpm = float(rpm)
+@app.route('/admin/exampleScenario1Loading', methods=['POST'])
+def exampleScenario1Loading():
+    timer = request.form['example1Length']
+    if timer:
+        return render_template('loading.html', timer=timer)
+    else:
+        return render_template('loading.html', timer=5)
+
+@app.route('/admin/exampleScenario1/<timer>', methods=['GET'])
+def exampleScenario1(timer):
+    cleanValues()
+    if timer.isdigit() == False:
+        return redirect('/admin')
+    global flow_rate
+    global p_ven
+    global svo2
+    timer = int(timer)
+    Vstatic = flow_rate
+    PvenStatic = p_ven
+    SvO2Static = svo2
+    startTime = time.time()
+    currentTimer = int(time.time() - startTime)
+    print(Vstatic)
+    while (currentTimer) <= timer:
+        flow_rate = Vstatic + (currentTimer/timer) * (65-Vstatic)
+        p_ven = PvenStatic + (currentTimer/timer) * (75-PvenStatic)
+        svo2 = SvO2Static + (currentTimer/timer) * (80-SvO2Static)
+        currentTimer = int(time.time() - startTime)
+    
+    return redirect('/admin')
+    
+@app.route('/admin/sliders', methods=['POST'])
+def sliders():
+    global flow_rate
+    global p_ven
+    global p_int
+    global delta_p
+    global p_art
+    global svo2
+    global t_art
+    print(request.form)
+    flow_rate = request.form['VS']
+    p_ven = request.form['PvenS']
+    p_int = request.form['PintS']
+    p_art = request.form['PartS']
+    delta_p = request.form['DeltaPS']
+    t_art = request.form['TartS']
+    svo2 = request.form['SvO2S']
+    cleanValues()
+    return redirect('/admin')
+    
+@app.route('/admin')
+def controlPanel():
+    global flow_rate
+    global p_ven
+    global p_int
+    global p_art
+    global t_art
+    global svo2
+    global delta_p
+    global rpm
+    cleanValues()
+    return render_template('admin.html', V=flow_rate, Pven=p_ven, Pint=p_int, Part=p_art, DeltaP=delta_p, Tart=t_art, SvO2=svo2)
+
+@app.route('/admin/about')
+def about():
+    return render_template('about.html')
+
+# Front end code
 def load_data():
     with open('data.json', 'r') as f:
         return json.load(f)
@@ -51,13 +142,10 @@ def dashboard():
     }
     return render_template('dashboard.html', rpm=rpm, flow_rate=flow_rate, p_ven=p_ven, p_int=p_int, p_art=p_art, t_art=t_art, svo2=svo2, delta_p=delta_p, safety_status=safety_status)
 
-@app.route('/admin')
-def admin():
-    return render_template("admin.html")
-
 @app.route('/update')
 def update():
     # Update example values (in a real application, these would be fetched from sensors or a database)
+    """
     global flow_rate, p_ven, p_int, p_art, t_art, svo2, delta_p, rpm
     flow_rate = round(random.uniform(2.0, 3.0), 2)
     p_ven = random.randint(-15, 15)
@@ -67,7 +155,7 @@ def update():
     svo2 = round(random.uniform(65.0, 95.0), 1)
     delta_p = random.randint(0, 20)
     rpm = random.randint(3500, 4500)
-
+    """
     safety_status = {
         'flow_rate': check_safety(flow_rate, safety_ranges['flow_rate']),
         'p_ven': check_safety(p_ven, safety_ranges['p_ven']),
